5b9e6291495cdc8a89d14ebecb0e1023a228fa74
frntdev99 <131940855+frntdev99@users.noreply.github.com>
2025-02-04 14:57:12 +0530

    Add files via upload

---
+++ mapping.js
@@ -0,0 +1,34 @@
+// mappings.js
+const countryCodeMapping = {
+  "Unknown": "Unknown",
+  "AF": "Afghanistan",
+  "AL": "Albania",
+  "DZ": "Algeria",
+  // ... add more as needed
+  "CR": "Costa Rica"
+};
+
+const usAreaCodes = {
+  "201": "New Jersey",
+  "202": "Washington D.C.",
+  "203": "Connecticut",
+  // ... add more as needed
+  "717": "Pennsylvania"
+};
+
+const CanadaCodes = {
+  "Unknown": "Unknown",
+  "AB": "Alberta",
+  "BC": "British Columbia",
+  "MB": "Manitoba",
+  "NB": "New Brunswick",
+  "NL": "Newfoundland and Labrador",
+  "NS": "Nova Scotia",
+  "NT": "Northwest Territories",
+  "NU": "Nunavut",
+  "ON": "Ontario",
+  "PE": "Prince Edward Island",
+  "QC": "Quebec",
+  "SK": "Saskatchewan",
+  "YK": "Yukon"
+};
