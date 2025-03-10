003d57c16e14c80b9003490cf7439181f5ba000c
frntdev99 <131940855+frntdev99@users.noreply.github.com>
2025-03-10 14:44:11 +0530

    Update exchange.js

--- exchange.js
+++ exchange.js
@@ -1,9 +1,418 @@
-const exchangeRates = {
-    "USD": 1,
-    "JPY": 0.0067, // Example rate (1 JPY = 0.0067 USD)
-    "EUR": 1.08,   // Example rate (1 EUR = 1.08 USD)
-    "GBP": 1.27    // Example rate (1 GBP = 1.27 USD)
+const otherexchangeRates = {
+    "AFN": 0.0137,  // 1 AFN = 0.0137 USD
+
+    "ALL": 0.0109,  // 1 ALL = 0.0109 USD
+
+    "DZD": 0.0074,  // 1 DZD = 0.0074 USD
+
+    "EUR": 1.087,   // 1 Andorran Euro = 1.087 USD
+
+    "AOA": 0.0015,  // 1 AOA = 0.0015 USD
+
+    "XCD": 0.370,   // 1 XCD (East Caribbean Dollar) = 0.370 USD
+
+    "ARS": 0.0034,  // 1 ARS = 0.0034 USD
+
+    "AMD": 0.0025,  // 1 AMD = 0.0025 USD
+
+    "AUD": 0.672,   // 1 AUD = 0.672 USD
+
+    "EUR": 1.087,   // 1 Euro (Austria) = 1.087 USD
+
+    "AZN": 0.588,   // 1 AZN = 0.588 USD
+
+    "BSD": 1.00,    // 1 BSD = 1.00 USD
+
+    "BHD": 2.63     // 1 BHD = 2.63 USD
+
+    "BDT": 0.012,  // Bangladeshi Taka
+
+    "BBD": 0.50,   // Barbadian Dollar
+
+    "BYN": 0.40,  // Belarusian Ruble
+
+    "EUR": 1.08,   // Euro
+
+    "BZD": 2.00,  // Belize Dollar
+
+    "BIF": 0.00055, // Burundian Franc
+
+    "BTN": 0.013,  // Bhutanese Ngultrum
+
+    "BOB": 0.14,  // Bolivian Boliviano
+
+    "BAM": 0.58,  // Bosnia and Herzegovina Convertible Mark
+
+    "BWP": 0.090,  // Botswanan Pula
+
+    "BRL": 0.19,  // Brazilian Real
+
+    "BND": 0.74,  // Brunei Dollar
+
+    "BGN": 0.58  // Bulgarian Lev    
+
+    "XOF": 0.0016,  // Burkina Faso CFA Franc (1 USD = 626.0000 XOF)
+
+    "BIF": 0.00034, // Burundi Franc (1 USD = 2,900.0000 BIF)
+
+    "KHR": 0.00025, // Cambodia Riel (1 USD = 4,015.0000 KHR)
+
+    "CFA": 0.0016,  // Cameroon CFA Franc (1 USD = 630.5500 CFA)
+
+    "CAD": 1.438,   // Canada Dollar (1 USD = 1.4380 CAD)
+
+    "CVE": 0.0094,  // Cape Verde Escudo (1 USD = 105.9900 CVE)
+
+    "KYD": 1.2195,  // Cayman Islands Dollar (1 USD = 0.8200 KYD)
+
+    "CFA": 0.0016,  // Central African Republic CFA Franc (1 USD = 630.5500 CFA)
+
+    "CFA": 0.0016,  // Chad CFA Franc (1 USD = 630.5500 CFA)
+
+    "CLP": 0.001,   // Chile Peso (1 USD = 992.6000 CLP)
+
+    "CNY": 0.137,   // China Renminbi (1 USD = 7.2990 CNY)
+
+    "COP": 0.00023  // Colombia Peso (1 USD = 4,402.4900 COP)
+
+    "KMF": 0.0023, // Comoros (KMF)
+
+    "CDF": 0.0018, // Republic of the Congo (CDF)
+
+    "CDF": 0.0005, // Democratic Republic of the Congo (CDF)
+
+    "XOF": 0.0012, // Ivory Coast (XOF)
+
+    "HRK": 0.15,   // Croatia (HRK)
+
+    "CUP": 1.00,   // Cuba (CUP)
+
+    "EUR": 1.12,   // Cyprus (EUR)
+
+    "CZK": 0.045,  // Czech Republic (CZK)
+
+    "DKK": 0.16,   // Denmark (DKK)
+
+    "DJF": 0.0058, // Djibouti (DJF)
+
+    "XCD": 0.56,   // Dominica (XCD)
+
+    "DOP": 0.01608, // Dominican Republic Peso (DOP)
+
+    "USD": 0.50,   // Ecuadorian Dollar (USD)
+
+    "EGP": 0.064,  // Egyptian Pound (EGP)
+
+    "USD": 0.46,   // Salvadoran Colón (USD)
+
+    "GNF": 0.0029, // Equatorial Guinean Ekwele (GNF)
+
+    "ERN": 0.067   // Eritrean Nakfa (ERN)
+
+    "EEK": 0.60,   // Estonian Kroon (EEK)
+
+    "ETB": 0.058,  // Ethiopian Birr (ETB)
+
+    "FJD": 0.47,   // Fijian Dollar (FJD)
+
+    "FIM": 0.18,   // Finnish Markka (FIM)
+
+    "FRF": 0.71,   // French Franc (FRF)
+
+    "XAF": 0.0018, // Gabonese CFA Franc (XAF)
+
+    "GMD": 0.024,  // Gambian Dalasi (GMD)
+
+    "GEL": 0.38,   // Georgia (1 GEL = 0.38 USD)
+
+    "EUR": 1.08,   // Germany (1 EUR = 1.08 USD)
+
+    "GHS": 0.056,  // Ghana (1 GHS = 0.056 USD)
+
+    "EUR": 1.08,   // Greece (1 EUR = 1.08 USD)
+
+    "XCD": 0.56,   // Grenada (1 XCD = 0.56 USD)
+
+    "GTQ": 0.13,   // Guatemala (1 GTQ = 0.13 USD)
+
+    "GNF": 0.00012, // Guinea (1 GNF = 0.00012 USD)
+
+    "GNF": 0.00012  // Guinea-Bissau (1 GNF = 0.00012 USD)
+
+    "GYD": 0.0048,  // Guyana (1 GYD = 0.0048 USD)
+
+    "HTG": 0.0085,  // Haiti (1 HTG = 0.0085 USD)
+
+    "HNL": 0.041,   // Honduras (1 HNL = 0.041 USD)
+
+    "HUF": 0.0031,  // Hungary (1 HUF = 0.0031 USD)
+
+    "ISK": 0.0075,  // Iceland (1 ISK = 0.0075 USD)
+
+    "INR": 0.012,   // India (1 INR = 0.012 USD)
+
+    "IDR": 0.066,   // Indonesia (1 IDR = 0.066 USD)
+
+    "IRR": 0.024,   // Iran (1 IRR = 0.024 USD)
+
+    "IQD": 0.00068, // Iraq (1 IQD = 0.00068 USD)
+
+    "EUR": 1.08,    // Ireland (1 EUR = 1.08 USD)
+
+    "ILS": 0.27,    // Israel (1 ILS = 0.27 USD)
+
+    "EUR": 1.08,    // Italy (1 EUR = 1.08 USD)
+
+    "JMD": 0.0066   // Jamaica (1 JMD = 0.0066 USD)
+
+    "JPY": 0.0067,  // Japan (1 JPY = 0.0067 USD)
+
+    "JOD": 0.141,   // Jordan (1 JOD = 0.141 USD)
+
+    "KZT": 0.0021,  // Kazakhstan (1 KZT = 0.0021 USD)
+
+    "KES": 0.0073,  // Kenya (1 KES = 0.0073 USD)
+
+    "AUD": 0.0089,  // Kiribati (1 AUD = 0.0089 USD)
+
+    "KRW": 0.00077, // South Korea (1 KRW = 0.00077 USD)
+
+    "KWD": 3.25,    // Kuwait (1 KWD = 3.25 USD)
+
+    "KGS": 0.011,   // Kyrgyzstan (1 KGS = 0.011 USD)
+
+    "LAK": 0.000057, // Laos (1 LAK = 0.000057 USD)
+
+    "EUR": 1.08,    // Latvia (1 EUR = 1.08 USD)
+
+    "LBP": 0.066,   // Lebanon (1 LBP = 0.066 USD)
+
+    "LSL": 0.053,   // Lesotho (1 LSL = 0.053 USD)
+
+    "LRD": 0.0064,  // Liberia (1 LRD = 0.0064 USD)
+
+    "LYD": 0.17,    // Libya (1 LYD = 0.17 USD)
+
+    "CHF": 1.08,    // Liechtenstein (1 CHF = 1.08 USD)
+
+    "EUR": 1.08,    // Lithuania (1 EUR = 1.08 USD)
+
+    "EUR": 1.08     // Luxembourg (1 EUR = 1.08 USD)
+
+    "MOP": 0.124,   // Macau (1 MOP = 0.124 USD)
+
+    "MKD": 0.018,   // North Macedonia (1 MKD = 0.018 USD)
+
+    "MGA": 0.00024, // Madagascar (1 MGA = 0.00024 USD)
+
+    "MWK": 0.0013,  // Malawi (1 MWK = 0.0013 USD)
+
+    "MYR": 0.22,    // Malaysia (1 MYR = 0.22 USD)
+
+    "MVR": 0.065,   // Maldives (1 MVR = 0.065 USD)
+
+    "XOF": 0.0016,  // Mali (1 XOF = 0.0016 USD)
+
+    "EUR": 1.08,    // Malta (1 EUR = 1.08 USD)
+
+    "USD": 1.00,    // Marshall Islands (1 USD = 1 USD; Dollarized economy)
+
+    "EUR": 1.08,    // Martinique (1 EUR = 1.08 USD)
+
+    "MRU": 0.026,   // Mauritania (1 MRU = 0.026 USD)
+
+    "MUR": 0.023,   // Mauritius (1 MUR = 0.023 USD)
+
+    "EUR": 1.08,    // Mayotte (1 EUR = 1.08 USD)
+
+    "MXN": 0.055,   // Mexico (1 MXN = 0.055 USD)
+
+    "USD": 1.00,    // Micronesia (1 USD = 1 USD; Dollarized economy)
+
+    "MDL": 0.052,   // Moldova (1 MDL = 0.052 USD)
+
+    "EUR": 1.08,    // Monaco (1 EUR = 1.08 USD)
+
+    "MNT": 0.00028, // Mongolia (1 MNT = 0.00028 USD)
+
+    "EUR": 1.08     // Montenegro (1 EUR = 1.08 USD)
+
+    "MAD": 0.10,   // Moroccan Dirham
+
+    "MZN": 0.016,  // Mozambican Metical
+
+    "MMK": 0.00049, // Myanmar Kyat
+
+    "ZAR": 0.052,  // South African Rand
+
+    "NPR": 0.0076, // Nepalese Rupee
+
+    "EUR": 1.08,   // Euro
+
+    "XPF": 1.08,   // CFP Franc (Pacific Franc)
+
+    "NZD": 0.62,   // New Zealand Dollar
+
+    "NIO": 0.028,  // Nicaraguan Córdoba
+
+    "NGN": 0.0021  // Nigerian Naira    
+
+    "NZD": 0.66,  // Niue (1 NZD = 0.66 USD)
+
+    "AUD": 1.00,  // Norfolk Island (1 AUD = 1.00 USD)
+
+    "NOK": 0.10,  // Norway (1 NOK = 0.10 USD)
+
+    "OMR": 2.60,  // Oman (1 OMR = 2.60 USD)
+
+    "PKR": 0.0058, // Pakistan (1 PKR = 0.0058 USD)
+
+    "USD": 1.00,  // Palau (1 USD = 1.00 USD; Dollarized economy)
+
+    "PAB": 1.00,  // Panama (1 PAB = 1.00 USD)
+
+    "PGK": 0.29,  // Papua New Guinea (1 PGK = 0.29 USD)
+
+    "PYG": 0.00014, // Paraguay (1 PYG = 0.00014 USD)
+
+    "PEN": 0.27,  // Peru (1 PEN = 0.27 USD)
+
+    "PHP": 0.018, // Philippines (1 PHP = 0.018 USD)
+
+    "PLN": 0.24,  // Poland (1 PLN = 0.24 USD)
+
+    "EUR": 1.08   // Portugal (1 EUR = 1.08 USD)
+
+    "USD": 1.00,  // Puerto Rico (1 USD = 1.00 USD; Dollarized economy)
+
+    "QAR": 0.27,  // Qatar (1 QAR = 0.27 USD)
+
+    "EUR": 1.08,  // Réunion (1 EUR = 1.08 USD)
+
+    "RON": 0.21,  // Romania (1 RON = 0.21 USD)
+
+    "RUB": 0.013, // Russia (1 RUB = 0.013 USD)
+
+    "RWF": 0.00091, // Rwanda (1 RWF = 0.00091 USD)
+
+    "EUR": 1.08,  // Saint Barthelemy (1 EUR = 1.08 USD)
+
+    "GBP": 1.26,  // Saint Helena (1 GBP = 1.26 USD)
+
+    "XCD": 0.56,  // Saint Kitts and Nevis (1 XCD = 0.56 USD)
+
+    "XCD": 0.56,  // Saint Lucia (1 XCD = 0.56 USD)
+
+    "EUR": 1.08   // Saint Martin (1 EUR = 1.08 USD)
+
+    "ANG": 0.56,  // Sint Maarten (1 ANG = 0.56 USD)
+
+    "STN": 0.046, // Sao Tome and Principe (1 STN = 0.046 USD)
+
+    "SAR": 0.27,  // Saudi Arabia (1 SAR = 0.27 USD)
+
+    "XOF": 0.0016, // Senegal (1 XOF = 0.0016 USD)
+
+    "RSD": 0.0092, // Serbia (1 RSD = 0.0092 USD)
+
+    "SCR": 0.075,  // Seychelles (1 SCR = 0.075 USD)
+
+    "SLL": 0.000056, // Sierra Leone (1 SLL = 0.000056 USD)
+
+    "SGD": 0.74,  // Singapore (1 SGD = 0.74 USD)
+
+    "EUR": 1.08,  // Slovakia (1 EUR = 1.08 USD)
+
+    "EUR": 1.08,  // Slovenia (1 EUR = 1.08 USD)
+
+    "SBD": 0.13   // Solomon Islands (1 SBD = 0.13 USD)
+
+    "SOS": 0.0017,  // Somalia (1 SOS = 0.0017 USD)
+
+    "ZAR": 0.054,   // South Africa (1 ZAR = 0.054 USD)
+
+    "KRW": 0.00077, // South Korea (1 KRW = 0.00077 USD)
+
+    "SSP": 0.0084,  // South Sudan (1 SSP = 0.0084 USD)
+
+    "EUR": 1.08,    // Spain (1 EUR = 1.08 USD)
+
+    "LKR": 0.0055,  // Sri Lanka (1 LKR = 0.0055 USD)
+
+    "SDG": 0.0022,  // Sudan (1 SDG = 0.0022 USD)
+
+    "SRD": 0.14,    // Suriname (1 SRD = 0.14 USD)
+
+    "NOK": 0.096,   // Svalbard and Jan Mayen (1 NOK = 0.096 USD)
+
+    "SZL": 0.053,   // Swaziland (1 SZL = 0.053 USD)
+
+    "SEK": 0.095,   // Sweden (1 SEK = 0.095 USD)
+
+    "CHF": 1.08     // Switzerland (1 CHF = 1.08 USD)
+
+    "SYP": 0.00038,  // Syria (1 SYP = 0.00038 USD)
+
+    "TWD": 0.032,    // Taiwan (1 TWD = 0.032 USD)
+
+    "TJS": 0.089,    // Tajikistan (1 TJS = 0.089 USD)
+
+    "TZS": 0.00043,  // Tanzania (1 TZS = 0.00043 USD)
+
+    "THB": 0.029,    // Thailand (1 THB = 0.029 USD)
+
+    "USD": 1.00,     // Timor-Leste (1 USD = 1 USD)
+
+    "TGH": 0.0016,   // Togo (1 XOF = 0.0016 USD)
+
+    "TKD": 0.00044,  // Tokelau (1 NZD = 0.00044 USD)
+
+    "TOP": 0.44,     // Tonga (1 TOP = 0.44 USD)
+
+    "TTD": 0.15,     // Trinidad and Tobago (1 TTD = 0.15 USD)
+
+    "TND": 0.36,     // Tunisia (1 TND = 0.36 USD)
+
+    "TRY": 0.037,    // Turkey (1 TRY = 0.037 USD)
+
+    "TMT": 0.29      // Turkmenistan (1 TMT = 0.29 USD)
+
+    "USD": 1.00,     // Turks and Caicos Islands (1 USD = 1 USD)
+
+    "AUD": 1.00,     // Tuvalu (1 AUD = 1 AUD)
+
+    "UGX": 0.00027,  // Uganda (1 UGX = 0.00027 USD)
+
+    "UAH": 0.027,    // Ukraine (1 UAH = 0.027 USD)
+
+    "AED": 0.27,     // United Arab Emirates (1 AED = 0.27 USD)
+
+    "GBP": 1.35,     // United Kingdom (1 GBP = 1.35 USD)
+
+    "USD": 1.00,     // United States (1 USD = 1 USD)
+
+    "UYU": 0.024,    // Uruguay (1 UYU = 0.024 USD)
+
+    "UZS": 0.000089, // Uzbekistan (1 UZS = 0.000089 USD)
+
+    "VUV": 0.012,    // Vanuatu (1 VUV = 0.012 USD)
+
+    "VES": 0.037     // Venezuela (1 VES = 0.037 USD)
+
+    "VND": 0.000043, // Vietnam (1 VND = 0.000043 USD)
+
+    "XPF": 0.0094,   // Wallis and Futuna (1 XPF = 0.0094 USD)
+
+    "MAD": 0.016,     // Western Sahara (1 MAD = 0.016 USD)
+
+    "YER": 0.0026,    // Yemen (1 YER = 0.0026 USD)
+
+    "ZMW": 0.053,     // Zambia (1 ZMW = 0.053 USD)
+
+    "ZWL": 0.012,     // Zimbabwe (1 ZWL = 0.012 USD)
+
+    "CRC": 0.0017    // Example rate (1 GBP = 1.27 USD)
 };
 
 // If using Vue.js, ensure it's globally available
-window.exchangeRates = exchangeRates;
+window.otherexchangeRates = otherexchangeRates;
